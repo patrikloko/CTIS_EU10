130a747d50b66e9b55c1142b7cd35c5712d0ee53
Lokman <lokmanozdmr2@gmail.com>
2023-03-11 00:17:57 +0300

    First project started

--- src/test/java/com/ctissolutions/runners/CukesRunner.java
+++ src/test/java/com/ctissolutions/runners/CukesRunner.java
@@ -1,2 +1,22 @@
-package com.ctissolutions.runners;public class CukesRunner {
+package com.ctissolutions.runners;
+
+import io.cucumber.junit.*;
+import org.junit.runner.*;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                //"pretty",
+                "html:target/cucumber-report.html",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber",
+        },
+        features = "src/test/resources/features",
+        glue = "com/ctissolutions/step_definitions",
+        dryRun = false,
+        tags = "",
+        publish = false
+
+)
+public class CukesRunner {
 }
