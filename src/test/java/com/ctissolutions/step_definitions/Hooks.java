2d61ddf943ae9246c05cd56566a8ddc8044d69cd
Lokman <lokmanozdmr2@gmail.com>
2023-03-12 21:54:25 +0300

    Hook class added updated

--- src/test/java/com/ctissolutions/step_definitions/Hooks.java
+++ src/test/java/com/ctissolutions/step_definitions/Hooks.java
@@ -12,45 +12,6 @@ In the class we will be able to pass pre- & post- conditions to
  each scenario and each step
  */
 public class Hooks {
-    //import from io.cucumber.java not from junit
-   //@Before(order = 1)
-    public void setupScenario() {
-        System.out.println("====Setting up browser using cucumber @Before");
-    }
-   // @Before(value = "@login", order = 2)
-    public void setupScenarioForLogins() {
-        System.out.println("====only apply to scenarios with @login tag");
-    }
-   // @Before(value = "@db", order = 0)
-    public void setupForDatabaseScenarios() {
-        System.out.println("====only apply to scenarios with @db tag");
-    }
-    @After
-    public void teardownScenario(Scenario scenario) {
-
-//scenario .is failed------> if scenario failed take screenshot if scenario pass no screenshot
-        if (scenario.isFailed()){
-
-            byte[] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
-            scenario.attach(screenshot,"image/png",scenario.getName());
-        }
-
-
-        Driver.closeDriver();
-
-
-      //  System.out.println("====Setting up browser using cucumber @After");
-      //  System.out.println("====Scenario ended/ Take a screenshot if failed!");
-
-    }
-   // @BeforeStep
-    public void setupStep() {
-        System.out.println("-------Applying setup using @BeforeStep");
-    }
-   // @AfterStep
-    public void afterStep() {
-        System.out.println("-------applying teardown using @AfterStep");
-    }
 
 
 }
