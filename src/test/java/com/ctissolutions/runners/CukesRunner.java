5ea7b17a5aaf8786cc620fb245e6afed59b4606b
Lokman <lokmanozdmr2@gmail.com>
2023-03-19 15:54:00 +0300

    json plugin added

--- src/test/java/com/ctissolutions/runners/CukesRunner.java
+++ src/test/java/com/ctissolutions/runners/CukesRunner.java
@@ -7,6 +7,7 @@ import org.junit.runner.*;
 @CucumberOptions(
         plugin = {
                 //"pretty",
+                "json:target/cucumber-report.json",
                 "html:target/cucumber-report.html",
                 "rerun:target/rerun.txt",
                 "me.jvt.cucumber.report.PrettyReports:target/cucumber",
