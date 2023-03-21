5ea7366138f5e69adf0bf3ff091952e09279b8a4
Lokman <lokmanozdmr2@gmail.com>
2023-03-21 20:49:28 +0300

    hooks takes screenshot method added

--- src/test/java/com/ctissolutions/step_definitions/Hooks.java
+++ src/test/java/com/ctissolutions/step_definitions/Hooks.java
@@ -12,6 +12,21 @@ In the class we will be able to pass pre- & post- conditions to
  each scenario and each step
  */
 public class Hooks {
+    @After
+    public void teardownScenario(Scenario scenario) {
+
+//scenario .is failed------> if scenario failed take screenshot if scenario pass no screenshot
+        if (scenario.isFailed()){
+
+            byte[] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot,"image/png",scenario.getName());
+        }
+
+
+        Driver.closeDriver();
+
+
+    }
 
 
 }
