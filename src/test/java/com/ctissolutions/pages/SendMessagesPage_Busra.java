a4630984776eab0e6012ee915f4a7daeab415ae4
Busra <m.busraaltnn@gmail.com>
2023-03-24 14:53:30 +0300

    SendMessage commit

---
+++ src/test/java/com/ctissolutions/pages/SendMessagesPage_Busra.java
@@ -0,0 +1,68 @@
+package com.ctissolutions.pages;
+
+import com.ctissolutions.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class SendMessagesPage_Busra {
+
+    public SendMessagesPage_Busra(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+
+    @FindBy(xpath = "//span[.='Send message …']")
+    public WebElement sendMessageField;
+
+    @FindBy(xpath = "//body[@contenteditable='true']")
+    public WebElement messageField;
+
+    @FindBy(xpath = "//button[@id='blog-submit-button-save']")
+    public WebElement sendButton;
+
+    @FindBy(xpath = "//button[@id='blog-submit-button-cancel']")
+    public WebElement cancelButton;
+
+    @FindBy(xpath = "(//span[.='All employees'])[2]")
+    public WebElement allEmployeesButton;
+
+    @FindBy(xpath = "//a[@id='bx-destination-tag']")
+    public WebElement addMoreButton;
+
+    @FindBy(xpath = "//span[@class='feed-add-post-del-but']")
+    public WebElement getAllEmployeesRemoveButton;
+
+    @FindBy(xpath = "//a[@id='destination1655006_last_U594']")
+    public WebElement anotherEmployeeToAdd;
+
+    @FindBy(xpath = "//span[.='The message title is not specified']")
+    public WebElement errorMessageForTitle;
+
+    @FindBy(xpath = "//span[.='Please specify at least one person.']")
+    public WebElement errorMessageForPerson;
+
+    @FindBy(xpath = "(//span[@class='feed-post-more-text'])[1]")
+    public WebElement moreButtonUnderMessage;
+
+    @FindBy(xpath="(//span[.='Delete'])[2]")
+    public WebElement deleteButtonUnderMore;
+
+    @FindBy(xpath = "//a[@id='bx-destination-tag']")
+    public WebElement addPersonButton;
+
+    @FindBy(xpath = "//a[@href='#U592']")
+    public WebElement addAnotherRecipents;
+
+    @FindBy(xpath = "//a[@id='destLastTab_rdL6v2']")
+    public WebElement recentButton;
+
+    @FindBy(xpath = "//a[@class='bx-finder-box-tab bx-lm-tab-department']")
+    public WebElement employeesAndDepatments;
+
+    @FindBy(xpath = "//a[@id='destEmailTab_rdL6v2']")
+    public WebElement emailUsers;
+
+
+
+}
---
+++ src/test/resources/features/SendMessageBusra.feature
@@ -0,0 +1,50 @@
+Feature:As a user, I should be able to send messages by clicking on Message tab under Active Stream.
+
+  Background:
+    Given the user should enter app with valid "https://qa.ctissolutions.com/" "helpdesk1@cybertekschool.com" "UserUser"
+
+
+
+  Scenario: User should be able to send a message by filling the mandatory fields.
+    Given user clicks the send message field
+    When user writes message
+    Then user clicks the send button
+
+
+  Scenario: User should not be able to send a message when user does not write message
+    Given user clicks the send message field
+    When user doesn't write message
+    When user clicks the send button
+    Then user should see The message title is not specified message
+
+
+  Scenario: User should not be able to send a message when user does not choose anybody and  remove default
+    Given user clicks the send message field
+    When user writes message
+    When user remove default All employees
+    When user clicks the send button
+    Then user should see Please specify at least one person. message
+
+  @sendMessage
+  Scenario: The message delivery should be to 'All employees' by default and should be changeable.
+    Given user clicks the send message field
+    When user writes messages
+    When user should see all employees by default for recipients
+    Then user remove default and add new more than one, changeable
+
+
+  Scenario: User should be able to cancel sending message at any time before sending
+    Given user clicks the send message field
+    When user writes message
+    Then user clicks the cancel button
+
+
+  Scenario: User should be able to delete message after sending
+    Given user clicks the send message field
+    When user writes the message
+    When user clicks the send button
+    Then user clicks more and then delete button to delete the message
+
+
+
+
