c1099da043110f7c262e783e30bdc61e41f4cec3
pinaraksoy <pinaraksoy_22@hotmail.com>
2023-03-31 00:16:20 +0300

    new elements located

--- src/test/java/com/ctissolutions/pages/SearchBoxPage_Pinar.java
+++ src/test/java/com/ctissolutions/pages/SearchBoxPage_Pinar.java
@@ -51,6 +51,10 @@ public class SearchBoxPage_Pinar {
     @FindBy(xpath = "//div[@data-id='field_EXTRANET']")
     public WebElement AddFieldExtraNet;
 
+    @FindBy(css = ".main-ui-control.main-ui-select[data-name='EXTRANET']")
+    public WebElement extraNetDropdown;
+
+
     @FindBy(xpath = "//div[@data-name='TAG']/div[2]/span")
     public WebElement removeTagField;
 
@@ -63,11 +67,17 @@ public class SearchBoxPage_Pinar {
     @FindBy(xpath = "(//div[.='This week'])[last()]")
     public WebElement dateThisWeek;
 
+    @FindBy(xpath = "//div[@title='Date: This week']/div")
+    public WebElement thisWeekTitle;
+
     @FindBy(xpath ="//span[@class='main-ui-item-icon main-ui-search']")
     public WebElement searchButton;
 
     @FindBy(xpath = "//div[@class='main-ui-select-inner-label'][text()='Posts']")
     public WebElement postsType;
+
+    @FindBy(xpath = "//div[@class='main-ui-square-item']")
+    public WebElement searchBoxTitle;
 
     @FindBy(xpath = "//div[@class='main-ui-control main-ui-multi-select']")
     public WebElement typeInputBox;
@@ -90,11 +100,17 @@ public class SearchBoxPage_Pinar {
     @FindBy(xpath = "//span[@class='ui-btn ui-btn-success main-ui-filter-field-button main-ui-filter-save']")
     public WebElement saveButton;
 
+    @FindBy(xpath = "//div[@class='main-ui-select-inner-label']")
+    public List<WebElement> fields;
 
+    @FindBy(xpath = "//div[@class='main-ui-item-icon-block main-ui-show']//span[@class='main-ui-item-icon main-ui-delete']")
+    public WebElement crossSignOfSearchBox;
 
+    @FindBy(xpath = "//span[@class='main-ui-filter-sidebar-item-text'][text()='groups']")
+    public WebElement groupFilter;
 
-
-
+    @FindBy(xpath = "//span[@class='main-ui-control-field-label']")
+    public List<WebElement> defaultFieldsNames;
 
 
 }
